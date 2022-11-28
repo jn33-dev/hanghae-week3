9e0930a24857b4bda0fbcfafa6176b22c598b87e
yena choi <geniusyena332@gmail.com>
2022-11-28 17:29:10 +0900

    강의자료를 lesson 폴더로 이동

--- schemas/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-  mongoose
-    .connect("mongodb://localhost:27017/spa_db")
-    .catch((err) => console.log("몽고디비 연결 에러", err));
-};
-
-module.exports = connect;
