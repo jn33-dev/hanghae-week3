d1b133223865acccfc3568206f329ad60c3fd945
yena choi <geniusyena332@gmail.com>
2022-11-29 17:53:11 +0900

    몽고디비 디비명 교체

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://127.0.0.1/spa_db", { useNewUrlParser: true })
+    .connect("mongodb://127.0.0.1/w3-personal", { useNewUrlParser: true })
     .catch((err) => console.log("몽고디비 연결 에러", err));
 };
 
