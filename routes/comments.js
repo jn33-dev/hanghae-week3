4c775a085078073bb11d3509870a9776bfa4724a
yena choi <geniusyena332@gmail.com>
2022-11-29 10:37:02 +0900

    comments.js 댓글 기능 1차 완성, 이후 리팩토링 진행 예정

--- routes/comments.js
+++ routes/comments.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Comments = require("../schemas/comment");
+const Posts = require("../schemas/post");
+
+/**
+ * 추가 작업 할 것
+ * req.params와 req.body 검증하는 함수 하나 짜고, 공통으로 사용
+ */
+
+function isParams()
 
 // 1.댓글 작성 api (postId, user, password, content, createdAt)
 router.post("/:_postId", async (req, res) => {
-  try {
-    const { _postId } = req.params;
-    const { user, password, content } = req.body;
-    console.log(content);
-    if (content == "" || content == undefined) {
-      res.status(400).send({ message: "댓글 내용을 입력해주세요." });
-    }
-    const createdAt = new Date().toString();
+  const { _postId } = req.params;
+  const { user, password, content } = req.body;
 
+  if (!content || !content.length)
+    return res.status(400).send({ message: "댓글 내용을 입력해주세요." });
+  try {
+    await Posts.findOne({ id: _postId });
+    const createdAt = new Date().toString();
     await Comments.create({
       postId: _postId,
       createdAt,
@@ -29,9 +36,10 @@ router.post("/:_postId", async (req, res) => {
 // 2. 댓글 목록 조회
 router.get("/:_postId", async (req, res) => {
   const { _postId } = req.params;
-  try {
-    const data = await Comments.find({ postId: _postId });
-    console.log(data);
+  const data = await Comments.find({ postId: _postId });
+  if (!data.length) {
+    res.status(400).send({ message: "데이터 형식이 올바르지 않습니다." });
+  } else {
     comments = [];
     for (let c of data) {
       console.log("c:", c);
@@ -40,10 +48,6 @@ router.get("/:_postId", async (req, res) => {
     }
     comments.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
     return res.json({ data: comments });
-  } catch (err) {
-    return res
-      .status(400)
-      .send({ message: "데이터 형식이 올바르지 않습니다." });
   }
 });
 
@@ -52,20 +56,20 @@ router.put("/:_commentId", async (req, res) => {
   const { _commentId } = req.params;
   const { password, content } = req.body;
 
-  if (!Object.keys(req.body).length || !_commentId.length) {
+  if (!content || !content.length)
+    return res.status(400).send({ message: "댓글 내용을 입력해주세요." });
+
+  if (!password.length) {
     return res
       .status(400)
       .send({ message: "데이터 형식이 올바르지 않습니다." });
-  }
-
-  if (!content || !content.length)
-    return res.status(400).send({ message: "댓글 내용을 입력해주세요." });
-  else {
+  } else {
     try {
-      await Comments.updateOne(
+      const data = await Comments.updateOne(
         { _id: _commentId, password: password },
         { $set: { content: content } }
       );
+      // console.log(data);
       return res.send({ message: "댓글을 성공적으로 수정하였습니다!" });
     } catch (err) {
       return res.status(404).send({ message: "댓글 조회에 실패하였습니다." });
@@ -78,7 +82,7 @@ router.delete("/:_commentId", async (req, res) => {
   const { _commentId } = req.params;
   const { password } = req.body;
 
-  if (!Object.keys(req.body).length || !_commentId.length) {
+  if (!password || !password.length) {
     return res
       .status(400)
       .send({ message: "데이터 형식이 올바르지 않습니다." });
