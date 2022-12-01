0d3a74a22f2258b2afc686660772d33ef5e2e703
yena choi <geniusyena332@gmail.com>
2022-12-01 11:01:06 +0900

    error handling 부분에서, 기존 Class로 정의한 각각의 에러를 하나의 CustomError로 통일하고, constructor()함수에 인자 값으로 에러 메세지와 status 넘버를 받아서 에러를 던지도록 처리-isBody()에서 검증하는 것은 매번 같은 조건문을 적지 않아서 편하지만, 한편으로는 가독성이 떨어지는 것 같기도 함

--- routes/comments.js
+++ routes/comments.js
@@ -3,41 +3,17 @@ const router = express.Router();
 const Comments = require("../schemas/comment");
 const Posts = require("../schemas/post");
 
-class BodyError {
-  constructor() {
-    this.name = "BodyError";
-    this.message = "데이터 형식이 올바르지 않습니다.";
-    this.status = 400;
-  }
-}
-
-class ContentError {
-  constructor() {
-    this.name = "ContentError";
-    this.message = "댓글 내용을 입력해주세요";
-    this.status = 400;
-  }
-}
-
-class CommentError {
-  constructor() {
-    this.name = "CommentError";
-    this.message = "댓글 조회에 실패했습니다.";
-    this.status = 404;
-  }
-}
-
-class IdentifierError {
-  constructor() {
-    this.name = "IdentifierError";
-    this.message = "게시글 조회에 실패했습니다.";
-    this.status = 404;
+class CustomError {
+  constructor(message, status) {
+    this.name = "CustomError";
+    this.message = message;
+    this.status = status;
   }
 }
 
 function isBody(req, res) {
   if (!Object.values(req.body).length || Object.values(req.body).includes("")) {
-    throw BodyError();
+    throw new CustomError("데이터 형식이 올바르지 않습니다.", 400);
   }
   return;
 }
@@ -47,11 +23,12 @@ router.post("/:_postId", async (req, res) => {
   try {
     const { _postId } = req.params;
     const { user, password, content } = req.body;
-    if (!content || !content.length) throw new ContentError();
+    if (!content || !content.length)
+      throw new CustomError("댓글 내용을 입력해주세요.", 404);
     await isBody(req, res);
-    const post = await Posts.findOne({ _id: _postId });
+    const post = await Posts.findOne({ _id: { $eq: _postId } });
     if (post === null) {
-      throw IdentifierError();
+      throw new CustomError("게시글 조회에 실패했습니다.", 404);
     }
     const createdAt = new Date().toISOString();
     await Comments.create({
@@ -64,7 +41,11 @@ router.post("/:_postId", async (req, res) => {
     return res.json({ message: "댓글을  생성하였습니다." });
   } catch (err) {
     console.log(err);
-    return res.status(err.status).send({ message: err.message });
+    if (!err.status) {
+      return res
+        .status(400)
+        .send({ message: "데이터 형식이 올바르지 않습니다." });
+    } else return res.status(err.status).send({ message: err.message });
   }
 });
 
@@ -72,21 +53,34 @@ router.post("/:_postId", async (req, res) => {
 router.get("/:_postId", async (req, res) => {
   try {
     const { _postId } = req.params;
-    const post = await Posts.findOne({ _id: _postId });
-    if (post === null) throw IdentifierError();
-    const data = await Comments.find({ postId: _postId });
-    if (!data.length) throw CommentError();
-
+    const post = await Posts.findOne({ _id: { $eq: _postId } });
+    if (post === null)
+      throw new CustomError("게시글 조회에 실패했습니다.", 404);
+    const comment = await Comments.find(
+      { postId: { $eq: _postId } },
+      { password: false }
+    ).sort({ createdAt: -1 });
     comments = [];
-    for (let c of data) {
-      const { _id, user, content, createdAt } = c;
-      comments.push({ commentId: _id, user, content, createdAt });
+    if (comment.length) {
+      comment.forEach((e) => {
+        comments.push({
+          postId: e["_id"],
+          user: e["user"],
+          content: e["content"],
+          createdAt: e["createdAt"],
+        });
+      });
+    } else {
+      comments.push("유효한 댓글 데이터가 없습니다.");
     }
-    comments.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
-    return res.json({ data: comments });
+    return res.status(200).json({ data: comments });
   } catch (err) {
     console.log(err);
-    return res.status(err.status).send({ message: err.message });
+    if (!err.status) {
+      return res
+        .status(400)
+        .send({ message: "데이터 형식이 올바르지 않습니다." });
+    } else return res.status(err.status).send({ message: err.message });
   }
 });
 
@@ -95,18 +89,25 @@ router.put("/:_commentId", async (req, res) => {
   try {
     const { _commentId } = req.params;
     const { password, content } = req.body;
-    if (!content || !content.length) throw ContentError;
+    if (!content || !content.length)
+      throw new CustomError("댓글 내용을 입력해주세요", 400);
     await isBody(req, res);
 
     const data = await Comments.findOneAndUpdate(
-      { _id: _commentId, password },
+      { _id: { $eq: _commentId }, password: { $eq: password } },
       { $set: { content } }
     );
-    if (data === null) throw CommentError;
-    return res.send({ message: "댓글을 성공적으로 수정하였습니다!" });
+    if (data === null) throw new CustomError("댓글 조회에 실패했습니다.", 404);
+    return res
+      .status(200)
+      .send({ message: "댓글을 성공적으로 수정하였습니다!" });
   } catch (err) {
     console.log(err);
-    return res.status(err.status).send({ message: err.message });
+    if (!err.status) {
+      return res
+        .status(400)
+        .send({ message: "데이터 형식이 올바르지 않습니다." });
+    } else return res.status(err.status).send({ message: err.message });
   }
 });
 
@@ -118,14 +119,18 @@ router.delete("/:_commentId", async (req, res) => {
     await isBody(req, res);
 
     const data = await Comments.findOneAndDelete({
-      _id: _commentId,
-      password: password,
+      _id: { $eq: _commentId },
+      password: { $eq: password },
     });
-    if (data === null) throw CommentError;
+    if (data === null) throw new CustomError("댓글 조회에 실패했습니다.", 404);
     return res.send({ message: "댓글을 성공적으로 삭제하였습니다!" });
   } catch (err) {
     console.log(err);
-    return res.status(err.status).send({ message: err.message });
+    if (!err.status) {
+      return res
+        .status(400)
+        .send({ message: "데이터 형식이 올바르지 않습니다." });
+    } else return res.status(err.status).send({ message: err.message });
   }
 });
 
